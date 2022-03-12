c5554290f78f99a25456785356796e8a6daebc26
Kartik Sethi <98469991+kartikDeveloper1@users.noreply.github.com>
2022-03-12 14:33:23 +0530

    For loop in Java With Scanner Class

---
+++ Basic/ForLoop.java
@@ -0,0 +1,19 @@
+/*
+How To use For loop in Java with taking input from User by Scanner Class.
+*/
+
+import java.util.Scanner;
+public class ForLoop {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int start=0,end=0;
+        System.out.println("Enter Staring value ");
+        start=sc.nextInt();
+        System.out.println("Enter Ending value ");
+        end=sc.nextInt();
+
+        for(int i=start;i<=end;i++){
+            System.out.println("Counting Value "+i);
+        }
+    }
+}
